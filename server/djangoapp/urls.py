19b351be9f9a8dd7ed0b36d5375f4e6554dd9ffe
stephanieLinley88 <stephaniecanning2018@gmail.com>
2026-08-21 14:58:19 +0100

    Add registration path to Django app URLs

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,6 +7,7 @@ app_name = 'djangoapp'
 
 urlpatterns = [
     # path for registration
+    path(route='register', view=views.registration, name='register'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
