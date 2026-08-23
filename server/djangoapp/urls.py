a7a2db58868fe4438f2045abf112c9f647708002
stephanieLinley88 <stephaniecanning2018@gmail.com>
2026-08-23 16:57:44 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
 from . import views
 
-app_name = 'djangoapp'
-
 urlpatterns = [
-    path(route='register', view=views.registration, name='register'),
-    path(route='login', view=views.login_user, name='login'),
-    path(route='logout', view=views.logout_request, name='logout'),
-    path(route='get_cars', view=views.get_cars, name='get_cars'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('register/', views.registration, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+
+    path('get_cars/', views.get_cars, name='get_cars'),
+
+    path('get_dealers/', views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>/', views.get_dealerships, name='get_dealers_by_state'),
+
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>/', views.get_dealer_reviews, name='dealer_reviews'),
+
+    path('add_review/', views.add_review, name='add_review'),
+]
